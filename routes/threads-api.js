6d2a23c3c8573fadbc2333353a9657e04e171332
Gurwinder Singh <threader.corp@gmail.com>
2024-11-17 08:41:51 +0000

    Fixing path issue

--- routes/threads-api.js
+++ routes/threads-api.js
@@ -122,9 +122,7 @@ router.get("/fetch-vid", async (req, res) => {
     '--no-default-browser-check',
   ],
   executablePath:
-      process.env.NODE_ENV === "production"
-        ? process.env.PUPPETEER_EXECUTABLE_PATH
-        : puppeteer.executablePath(),
+    puppeteer.executablePath(),
         });
     // Create a new page and setup interception
     const page = await browser.newPage();
